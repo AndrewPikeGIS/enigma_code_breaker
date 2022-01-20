4653c0923ea2da889f610754c0005011597ac1e1
Andrew Pike <andrewpikegis@gmail.com>
2022-01-19 23:36:13 -0700

    added enigma file and plain text plan

---
+++ enigma_cypher_machine.py
@@ -0,0 +1,10 @@
+#enigma build
+#entry string
+#right hand rotor rotates
+#plugboard swap for 10 pairs
+#enter through entry wheel
+#pass through 3 rotors
+#hit reflector scramble again.
+#pass back through three rotors
+#plug board swap for 10 same 10 pairs
+
