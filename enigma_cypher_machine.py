fddec081d7a684cb03740d24f55c1b6d1a341afb
Andrew Pike <andrewpikegis@gmail.com>
2022-01-23 22:34:43 -0700

    updated rotor pair function to prevent chars encoded as themselves

--- enigma_cypher_machine.py
+++ enigma_cypher_machine.py
@@ -11,31 +11,34 @@
 import random, string
 
 def build_rotor_pairs(rotor = 1):
-    random.seed = rotor
-
-    
     keys = []
     values = []
     
     letters = string.ascii_lowercase
+    pair = letters
+    
+    random.seed = rotor
     
     for x in range(26):
         keys.append(letters[x])
-        values.append(random.choice(letters))
-    
+        pair = pair.replace(letters[x], "")
+        for val in values:
+            pair = pair.replace(val, "")
+        values.append(pair[random.randint(0,len(pair)-1)])
+        pair = letters
     
     dct_rotor_pairs = dict(zip(keys, values))
     
     return(dct_rotor_pairs)
     
 class enigma_rotor:
-    def __init__(self, position, rotor_pairs):
+    def __init__(self, position, rotor_num):
         self.position = position
 
-        self.rotor_pairs = rotor_pairs
+        self.rotor_pairs = build_rotor_pairs(rotor_num)
 
 test = enigma_rotor('right', build_rotor_pairs(1))
 
 print(test.position)
 
-print(build_rotor_pairs())
+print(test.rotor_pairs)
