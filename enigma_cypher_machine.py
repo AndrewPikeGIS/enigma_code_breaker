6b361e7d30a86af0ca4e0171e38114397357fd80
Andrew Pike <andrewpikegis@gmail.com>
2022-01-23 21:22:41 -0700

    Added rotor pair function and enigma class def

--- enigma_cypher_machine.py
+++ enigma_cypher_machine.py
@@ -8,3 +8,34 @@
 #pass back through three rotors
 #plug board swap for 10 same 10 pairs
 
+import random, string
+
+def build_rotor_pairs(rotor = 1):
+    random.seed = rotor
+
+    
+    keys = []
+    values = []
+    
+    letters = string.ascii_lowercase
+    
+    for x in range(26):
+        keys.append(letters[x])
+        values.append(random.choice(letters))
+    
+    
+    dct_rotor_pairs = dict(zip(keys, values))
+    
+    return(dct_rotor_pairs)
+    
+class enigma_rotor:
+    def __init__(self, position, rotor_pairs):
+        self.position = position
+
+        self.rotor_pairs = rotor_pairs
+
+test = enigma_rotor('right', build_rotor_pairs(1))
+
+print(test.position)
+
+print(build_rotor_pairs())
