475c22e934ce347a5f935ef5bc67d738ceb36298
Andrew Pike <andrewpikegis@gmail.com>
2022-02-09 23:23:23 -0700

    built working test for rotor rotate

--- tests/test_rotor.py
+++ tests/test_rotor.py
@@ -1,5 +1,11 @@
-import enigma_code.enigma_cypher_machine as enigma
+from pickle import TRUE
+import pytest
+
+import enigma_code_breaker.enigma_code.enigma_cypher_machine as enigma
 
 
 def test_rotate():
-    assert TRUE
+    test_rotor = enigma.EnigmaRotor(1, 1, 1)
+    test_rotor.rotate_rotor()
+
+    assert test_rotor.position == 2
