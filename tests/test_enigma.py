4c277160b037b0b10a944d5975fc6d09837db0b6
Andrew Pike <andrewpikegis@gmail.com>
2022-02-13 23:06:38 -0700

    added plug board test

---
+++ tests/test_enigma.py
@@ -0,0 +1,24 @@
+import pytest
+
+import enigma_code_breaker.enigma_code.enigma_cypher_machine as enigma
+
+
+def test_plugboard():
+
+    plugboard_test = {
+        "a": "j",
+        "g": "k",
+        "d": "b",
+        "t": "f",
+        "e": "w",
+        "h": "a",
+        "p": "c",
+        "o": "r",
+        "y": "e",
+        "n": "g"
+    }
+
+    test_enigma = enigma.EnigmaMachine(1, 1, 1, plugboard_test)
+
+    assert "t" in test_enigma.plug_board_pairs.keys()
+    assert "j" not in test_enigma.plug_board_pairs.keys()
