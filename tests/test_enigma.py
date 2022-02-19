adce30b6d15ae13e636241d266d39066ecb652a5
Andrew Pike <andrewpikegis@gmail.com>
2022-02-18 17:02:50 -0700

    added test for plug board and one rotor

--- tests/test_enigma.py
+++ tests/test_enigma.py
@@ -1,3 +1,4 @@
+from cgi import test
 import pytest
 
 import enigma_code_breaker.enigma_code.enigma_cypher_machine as enigma
@@ -22,7 +23,7 @@ test_enigma = enigma.EnigmaMachine(1, 1, 1, plugboard_test)
 
 def test_plugboard():
 
-    assert "t" in test_enigma.plug_board_pairs.keys()
+    assert "f" in test_enigma.plug_board_pairs.keys()
     assert "j" not in test_enigma.plug_board_pairs.keys()
     assert "j" in test_enigma.plug_board_pairs.values()
 
@@ -52,9 +53,47 @@ def test_plugboard_in_same_as_reverse():
 
     assert string_in == string_out
 
+# test that no duplicated values exist in plugboard
+
 
 def test_no_duplicated_values_in_plugboard():
     plugboard_keys = list(test_enigma.plug_board_pairs.keys())
     plugboard_values = list(test_enigma.plug_board_pairs.values())
 
     assert len(set(plugboard_keys) & set(plugboard_values)) == 0
+
+# test that reflector forward and backwards returns same result.
+
+
+def test_relector_same_forward_back():
+    string_in = "b"
+
+    string_encrypt = test_enigma.reflector.rotor_encryption_forward(
+        string_in)
+
+    string_out = test_enigma.reflector.rotor_encryption_backward(
+        string_encrypt)
+
+    assert string_in == string_out
+
+
+# test that forward backwards through plugboard and a rotor returns same letter.
+
+def test_plug_to_rotor_and_back():
+    string_in = "d"
+
+    string_encrypt = test_enigma.encrypt_char_plugboard(string_in)
+
+    # run current char through rotor 1
+    char_rotor1_out = test_enigma.rotor_1.rotor_encryption_forward(
+        string_encrypt
+    )
+
+    char_rotor1_back = test_enigma.rotor_1.rotor_encryption_backward(
+        char_rotor1_out
+    )
+
+    string_out = test_enigma.encrypt_char_plugboard(char_rotor1_back)
+    message = "P1: " + string_encrypt + "\nR1: " + char_rotor1_out + \
+        "\nR1B: " + char_rotor1_back + "\nP1B: " + string_out
+    assert string_in == string_out, message
