b84ca298e2d0ba49247fcf4bcda0eca5e3ac5638
Andrew Pike <andrewpikegis@gmail.com>
2022-02-19 21:07:53 -0700

    added test for plug to rotor to reflector and back

--- tests/test_enigma.py
+++ tests/test_enigma.py
@@ -1,5 +1,6 @@
 from cgi import test
 import pytest
+from string import ascii_lowercase
 
 import enigma_code_breaker.enigma_code.enigma_cypher_machine as enigma
 
@@ -80,7 +81,7 @@ def test_relector_same_forward_back():
 # test that forward backwards through plugboard and a rotor returns same letter.
 
 def test_plug_to_rotor_and_back():
-    string_in = "d"
+    string_in = "h"
 
     string_encrypt = test_enigma.encrypt_char_plugboard(string_in)
 
@@ -94,6 +95,42 @@ def test_plug_to_rotor_and_back():
     )
 
     string_out = test_enigma.encrypt_char_plugboard(char_rotor1_back)
+
     message = "P1: " + string_encrypt + "\nR1: " + char_rotor1_out + \
         "\nR1B: " + char_rotor1_back + "\nP1B: " + string_out
+
     assert string_in == string_out, message
+
+
+def test_plug_to_rotor_to_reflector_back():
+    letters = ascii_lowercase
+
+    for string_char in letters:
+
+        string_encrypt = test_enigma.encrypt_char_plugboard(string_char)
+
+        # run current char through rotor 1
+        char_rotor1_out = test_enigma.rotor_1.rotor_encryption_forward(
+            string_encrypt
+        )
+
+        # run current char through reflector
+        char_reflector_out = test_enigma.reflector.rotor_encryption_forward(
+            char_rotor1_out
+        )
+
+        char_reflector_back = test_enigma.reflector.rotor_encryption_backward(
+            char_reflector_out
+        )
+
+        char_rotor1_back = test_enigma.rotor_1.rotor_encryption_backward(
+            char_reflector_back
+        )
+
+        string_out = test_enigma.encrypt_char_plugboard(char_rotor1_back)
+
+        message = "P1: " + string_encrypt + "\nR1: " + char_rotor1_out + \
+            "\nReF: " + char_reflector_out + "\nReB: " + char_reflector_back + \
+            "\nR1B: " + char_rotor1_back + "\nP1B: " + string_out
+
+        assert string_char == string_out, message
