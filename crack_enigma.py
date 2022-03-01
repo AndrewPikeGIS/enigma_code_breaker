29f03606aee063cc65409859c580cb657721732a
Andrew Pike <andrewpikegis@gmail.com>
2022-02-28 23:28:27 -0700

    updated set_known_value method

--- crack_enigma.py
+++ crack_enigma.py
@@ -8,12 +8,34 @@ class Victory:
         with open(txt_path, "r") as txt_file:
             self.encrypted_message = txt_file.read()
 
+        self.known_value = " " * len(self.encrypted_message)
+
     def __check_private(self):
         self.test = self.encrypted_message.upper()
+
+    def known_val_decrypt(self):
+        # decryption method given a known number of values.
+        pass
+
+    def set_known_value(self, value, start_position):
+
+        known_val = self.known_value
+
+        length_replacement = len(value)
+
+        known_val = known_val[:start_position] + value + \
+            known_val[start_position + length_replacement:]
+
+        self.known_value = known_val
 
 
 VictoryTest = Victory()
 
 VictoryTest.read_encrypted_text(r"encrypted_commands/command1.txt")
 
-print(VictoryTest.test)
+VictoryTest.set_known_value("Good Morning,\n\nWeather today", 0)
+
+VictoryTest.set_known_value(
+    "Hail Hitler.", len(VictoryTest.encrypted_message)-11)
+
+print(VictoryTest.known_value)
