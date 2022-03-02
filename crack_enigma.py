fcbddf6b003333f4e6f1495a4699005f7bd7191e
Andrew Pike <andrewpikegis@gmail.com>
2022-03-01 22:25:00 -0700

    added calls to the functions for iterating the enigma machine

--- crack_enigma.py
+++ crack_enigma.py
@@ -1,8 +1,12 @@
+from sympy import E
+from enigma_code.enigma_cypher_machine import EnigmaMachine
+
+
 class Victory:
     def __init__(self):
         self.encrypted_message = ""
 
-        self.test = ""
+        self.build_enigma_machine()
 
     def read_encrypted_text(self, txt_path):
         with open(txt_path, "r") as txt_file:
@@ -10,12 +14,30 @@ class Victory:
 
         self.known_value = " " * len(self.encrypted_message)
 
-    def __check_private(self):
-        self.test = self.encrypted_message.upper()
+    def build_enigma_machine(self):
 
-    def known_val_decrypt(self):
-        # decryption method given a known number of values.
-        pass
+        self.rotor1seed = 1
+        self.rotor2seed = 1
+        self.rotor3seed = 1
+
+        self.rotor1start = 1
+        self.rotor2start = 1
+        self.rotor3start = 1
+
+        self.plugboard = {
+            "a": "b",
+            "c": "d",
+            "e": "f",
+            "g": "h",
+            "i": "j",
+            "k": "l",
+            "m": "n",
+            "o": "p",
+            "q": "r",
+            "s": "t"}
+
+        self.Enigma = EnigmaMachine(plugboard, rotor1seed, rotor1start,
+                                    rotor2seed, rotor2start, rotor3seed, rotor3start)
 
     def set_known_value(self, value, start_position):
 
@@ -27,6 +49,27 @@ class Victory:
             known_val[start_position + length_replacement:]
 
         self.known_value = known_val
+
+    def check_output_on_known_val(self):
+        # check the decrypted string against the known value string and give a score
+        pass
+
+    def decrypt_with_known_value(self):
+        # code to decrypt the string when there are some known values.
+
+        pass
+
+    def interate_on_starting_positions(self):
+        # iterate the enigma starting positions
+        pass
+
+    def iterate_on_rotor_seed(self):
+        # iterate the enigma seeds
+        pass
+
+    def iterate_on_plugboard(self):
+        # iterate the plugboard
+        pass
 
 
 VictoryTest = Victory()
